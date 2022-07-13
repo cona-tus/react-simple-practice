2b6059305634d8c4b9be242994c41eece590acbe
Conatus <cona_tus@naver.com>
2022-07-13 21:36:53 +0900

    Event: Edit TOC Component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       mode: 'read',
+      selected_content_id: 2,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: '👋 Hello, React!' },
       contents: [
@@ -29,8 +29,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
 
     return (
@@ -42,7 +50,15 @@ class App extends Component {
             this.setState({ mode: 'welcome' });
           }.bind(this)}
         ></Subject>
-        <TOC data={this.state.contents}></TOC>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id),
+            });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
