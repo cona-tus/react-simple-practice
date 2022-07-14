a0ef7390f36d6a07d42c552243a173ff812e62b4
Conatus <cona_tus@naver.com>
2022-07-14 14:59:59 +0900

    Create: Display mode when it changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 import './App.css';
@@ -24,11 +25,13 @@ class App extends Component {
   render() {
     console.log('App render');
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
 
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === 'read') {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -40,6 +43,9 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>;
     }
 
     return (
@@ -67,7 +73,7 @@ class App extends Component {
             });
           }.bind(this)}
         ></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div>
     );
   }
